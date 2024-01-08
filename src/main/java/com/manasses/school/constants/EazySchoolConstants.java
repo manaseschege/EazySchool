fc440ca7fce61db1d8c45068dd6e62d92d8ab7f6
manasses <manaseschege0@gmail.com>
2024-01-08 07:58:03 +0300

    added data in mysql db

--- src/main/java/com/manasses/school/constants/EazySchoolConstants.java
+++ src/main/java/com/manasses/school/constants/EazySchoolConstants.java
@@ -2,7 +2,7 @@ package com.manasses.school.constants;
 
 public interface EazySchoolConstants {
 
-    public static final String ANONYMOUS = "Anonymous";
+
     public static final String OPEN = "Open";
     public static final String CLOSE = "Close";
 }
