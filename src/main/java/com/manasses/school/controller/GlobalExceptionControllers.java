a89566cbfc7cda16da02cf5c35c0329a382fe07b
manasses <manaseschege0@gmail.com>
2024-01-24 12:17:37 +0300

    remove enable uditing feature

--- src/main/java/com/manasses/school/controller/GlobalExceptionControllers.java
+++ src/main/java/com/manasses/school/controller/GlobalExceptionControllers.java
@@ -1,12 +1,13 @@
 package com.manasses.school.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
-@ControllerAdvice
+@ControllerAdvice(annotations = Controller.class)
 public class GlobalExceptionController {
 @ExceptionHandler(Exception.class)
 //@ExceptionHandler will register the given method for a given
